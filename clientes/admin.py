8bb01e80f103558106b2554ac625eecb0d245162
Lucas Bastos <lucsbasto@gmail.com>
2018-07-24 19:42:42 -0300

    eita

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Doc
 
 admin.site.register(Person)
+admin.site.register(Doc)
--- clientes/models.py
+++ clientes/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Doc(models.Model):
+    num_doc = models.CharField(max_length=9);
+    def __str__(self):
+        return self.num_doc
+
+
 class Person(models.Model):
     firstName = models.CharField(max_length=20);
     lastName = models.CharField(max_length=20);
@@ -8,7 +14,7 @@ class Person(models.Model):
     bio = models.TextField();
     photo = models.ImageField(upload_to='client_photos', null=True, blank=True);
     # client_photos é a pasta que vai ser criada, null e blank é pra deixar opcional
-
+    document = models.OneToOneField(Doc, null=True, blank=True, on_delete=models.CASCADE);
 
     def __str__(self):
         return self.firstName;
