c65e2eb54ce2a901b64e8f7ba8313d8ad436abf8
Lucas Bastos <lucsbasto@gmail.com>
2018-07-24 13:36:41 -0300

    more fragments

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestao_clientes',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
